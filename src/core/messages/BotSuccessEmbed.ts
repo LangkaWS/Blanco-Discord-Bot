1c473c516ca2854ba4e22b4d79fddcca9d7e1267
Langka <langkaws@outlook.com>
2023-02-19 13:31:22 +0100

    Add bot success embed class to display success messages

---
+++ src/core/messages/BotSuccessEmbed.ts
@@ -0,0 +1,20 @@
+import { HexColorString } from 'discord.js';
+import { Constants } from '../Constants';
+import { BotEmbed } from './BotEmbed';
+
+export class BotSuccessEmbed extends BotEmbed {
+
+	constructor(message: string) {
+		super();
+		this.setSuccessColor();
+		this.setDescription(message);
+	}
+
+	/**
+	 * Set the success color
+	 */
+	private setSuccessColor() {
+		this.setColor(<HexColorString>Constants.MESSAGES.COLORS.SUCCESS);
+	}
+
+}
